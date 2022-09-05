61375d8a963e6d2956ecafe72ded236554d3edca
Jasham <jasham009@gmail.com>
2022-09-05 19:38:55 +0530

    updating code

--- src/components/star/index.tsx
+++ src/components/star/index.tsx
@@ -5,7 +5,13 @@ import styles from "../../../styles/Star.module.css";
 export const Star = () => {
   return (
     <div className={styles.star}>
-      <Image src={"/images/icon-star.svg"} width={15} height={15} alt="star" />
+      <Image
+        src={"/images/icon-star.svg"}
+        width={15}
+        height={15}
+        alt="star"
+        loading="eager"
+      />
     </div>
   );
 };
