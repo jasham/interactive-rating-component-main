05cb1280b9c7677ba8f441ca702dc228070e9c8d
Jasham <jasham009@gmail.com>
2022-09-05 18:47:28 +0530

    updating code

--- src/components/rating/index.tsx
+++ src/components/rating/index.tsx
@@ -1,31 +1,31 @@
-// import React from "react";
-// import styles from "../../../styles/Rating.module.css";
+import React from "react";
+import styles from "../../../styles/Rating.module.css";
 
-// type RatingProps = {
-//   onSelectRadio: (id: number) => void;
-// };
+type RatingProps = {
+  onSelectRadio: (id: number) => void;
+};
 
-// const ratingList = [1, 2, 3, 4, 5];
-// export const Rating = ({ onSelectRadio }: RatingProps) => {
-//   return (
-//     <div className={styles.rating}>
-//       {ratingList.map((data, index) => (
-//         <label
-//           key={index.toString()}
-//           className={styles.radio}
-//           htmlFor={index.toString()}
-//         >
-//           <input
-//             id={index.toString()}
-//             type="radio"
-//             name="rating"
-//             value={data}
-//             className={styles.inputRadio}
-//             onClick={() => onSelectRadio(data)}
-//           />
-//           <span className={styles.ratingCheck}>{data}</span>
-//         </label>
-//       ))}
-//     </div>
-//   );
-// };
+const ratingList = [1, 2, 3, 4, 5];
+export const Rating = ({ onSelectRadio }: RatingProps) => {
+  return (
+    <div className={styles.rating}>
+      {/* {ratingList.map((data, index) => (
+        <label
+          key={index.toString()}
+          className={styles.radio}
+          htmlFor={index.toString()}
+        >
+          <input
+            id={index.toString()}
+            type="radio"
+            name="rating"
+            value={data}
+            className={styles.inputRadio}
+            onClick={() => onSelectRadio(data)}
+          />
+          <span className={styles.ratingCheck}>{data}</span>
+        </label>
+      ))} */}
+    </div>
+  );
+};
