d36695b0b65fc7e384e10ced4a348810ab9c3326
Jasham <jasham009@gmail.com>
2022-09-05 19:48:04 +0530

    updating code

--- src/modules/rating/index.tsx
+++ src/modules/rating/index.tsx
@@ -23,7 +23,7 @@ export const RatingMain = () => {
       <div className={styles.ratingWrapper}>
         <Star />
         <div className={styles.head}>
-          <h2>How did we do?</h2>
+          <h1>How did we do?</h1>
           <p>
             Please let us know how we did with your support request. All
             feedback is appreciated to help us improve our offerings!
